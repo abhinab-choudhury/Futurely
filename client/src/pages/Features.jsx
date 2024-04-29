a2f3f761354e548dc0232e2ac73b077455db16f9
Arkadeep18 <arkadeepk8@gmail.com>
2024-04-29 17:33:29 +0530

    File Updated

--- client/src/pages/Features.jsx
+++ client/src/pages/Features.jsx
@@ -12,10 +12,10 @@ function Features() {
         <section>
           <Navbar />
         </section>
-        <section className='flex flex-col justify-center relative top-10 h-fit m-10'>
+        <section className='flex flex-col justify-center relative top-10 h-fit m-8'>
           <h1 className='tablet:text-4xl mobile:text-3xl w-[80vw] font-extrabold laptop:text-7xl flex ml-auto mr-auto justify-center'>Features</h1>
-          <hr className='border-4 border-black w-[80vw] ml-auto mr-auto my-10' />
-          <div className='w-[80vw] mobile:block laptop:flex laptop:align-middle justify-between ml-auto mr-auto mt-10 border-black border-4 rounded-[30px] p-5'>
+          <hr className='border-4 border-black w-[80vw] ml-auto mr-auto my-4' />
+          <Link to='/features/marriagecall' className='hover:text-white hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 w-[80vw] mobile:block laptop:flex laptop:align-middle justify-between ml-auto mr-auto mt-10 border-black border-4 rounded-[30px] p-5'>
             <div className='laptop:mx-10 laptop:w-fit laptop:my-auto laptop:text-3xl font-bold'>
               Marriage Call
             </div>
@@ -23,11 +23,11 @@ function Features() {
               <span className='phone:text-xl laptop:text-2xl font-semibold'>Uncover your love story:</span><br />
               Discover if the stars align for love or tradition with our numerology prediction.
             </div>
-            <Link to='/features/marriagecall' className='laptop:mx-10 laptop:my-auto tablet:my-auto'>
+            <button className='laptop:mx-10 laptop:my-auto tablet:my-auto'>
               <img src={RightBtn} alt='Button.png' />
-            </Link>
-          </div>
-          <div className='w-[80vw] mobile:block laptop:flex laptop:align-middle justify-between ml-auto mr-auto mt-10 border-black border-4 rounded-[30px] p-5'>
+            </button>
+          </Link>
+          <Link to='/features/sunshinharmony' className='hover:text-white hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 w-[80vw] mobile:block laptop:flex laptop:align-middle justify-between ml-auto mr-auto mt-10 border-black border-4 rounded-[30px] p-5'>
             <div className='laptop:mx-10 laptop:w-fit laptop:my-auto laptop:text-3xl font-bold'>
               Sunshine Harmony
             </div>
@@ -35,11 +35,11 @@ function Features() {
               <span className='phone:text-xl laptop:text-2xl font-semibold'>Sun Sign Compatibility Check: </span><br />
               See if your sun signs are destined to shine together.
             </div>
-            <Link to='/features/sunshinharmony' className='laptop:mx-10 laptop:my-auto tablet:my-auto'>
+            <button className='laptop:mx-10 laptop:my-auto tablet:my-auto'>
               <img src={RightBtn} alt='Button.png' />
-            </Link>
-          </div>
-          <div className='w-[80vw] mobile:block laptop:flex laptop:align-middle justify-between ml-auto mr-auto mt-10 border-black border-4 rounded-[30px] p-5'>
+            </button>
+          </Link>
+          <Link to='/features/sunsign' className='hover:text-white hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 w-[80vw] mobile:block laptop:flex laptop:align-middle justify-between ml-auto mr-auto mt-10 border-black border-4 rounded-[30px] p-5'>
             <div className='laptop:mx-10 laptop:my-auto laptop:text-3xl font-bold laptop:w-fit'>
               Sun Sign
             </div>
@@ -47,10 +47,10 @@ function Features() {
               <span className='phone:text-xl laptop:text-2xl font-semibold'>Cosmic Connections: </span><br />
               Explore the harmony between your sun signs.
             </div>
-            <Link to='/features/sunsign' className='laptop:mx-10 laptop:my-auto tablet:my-auto'>
+            <button className='laptop:mx-10 laptop:my-auto tablet:my-auto'>
               <img src={RightBtn} alt='Button.png' />
-            </Link>
-          </div>
+            </button>
+          </Link>
         </section>
         <section>
           <Footer />
--- client/src/pages/SunsignHarmonyPage.jsx
+++ client/src/pages/SunsignHarmonyPage.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 function SunsignHarmonyPage() {
   return (
     <>
-      <div className='mt-20'>
-        <h1 className='text-3xl'> Sunsign Harmony</h1>
-      </div>
+      <main>
+        <section>
+          <Navbar />
+        </section>
+        <section>
+          <div className='mt-20'>
+            <h1 className='text-3xl'> Sunsign Harmony</h1>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
